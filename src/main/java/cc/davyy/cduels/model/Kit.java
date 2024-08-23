b58703cb6f8c8c6e4d92a0d887170b498a216671
davide <dav.grigioni@gmail.com>
2024-08-23 22:53:04 +0200

    Added Kit of method

--- src/main/java/cc/davyy/cduels/model/Kit.java
+++ src/main/java/cc/davyy/cduels/model/Kit.java
@@ -4,4 +4,8 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
-public record Kit(String name, List<ItemStack> items, String permission) {}
+public record Kit(String name, List<ItemStack> items, String permission) {
+
+    public static Kit of(String name, List<ItemStack> items, String permission) { return new Kit(name, items, permission); }
+
+}
