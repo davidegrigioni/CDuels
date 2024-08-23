e869d16e18c33a3533273357ed608ec9bd9b6d20
davide <dav.grigioni@gmail.com>
2024-08-24 01:14:17 +0200

    binded rewardmanager

--- src/main/java/cc/davyy/cduels/module/CModule.java
+++ src/main/java/cc/davyy/cduels/module/CModule.java
@@ -1,10 +1,7 @@
 package cc.davyy.cduels.module;
 
 import cc.davyy.cduels.CDuels;
-import cc.davyy.cduels.managers.DatabaseManager;
-import cc.davyy.cduels.managers.DuelManager;
-import cc.davyy.cduels.managers.KitManager;
-import cc.davyy.cduels.managers.WorldCreatorManager;
+import cc.davyy.cduels.managers.*;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
@@ -25,6 +22,7 @@ public class CModule extends AbstractModule {
 
         this.bind(WorldCreatorManager.class).in(Singleton.class);
         this.bind(DuelManager.class).in(Singleton.class);
+        this.bind(RewardManager.class).in(Singleton.class);
     }
 
 }
