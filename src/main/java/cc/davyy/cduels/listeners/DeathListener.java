108cfa1870fbd080c656cf1dc848cbddabb48702
davide <dav.grigioni@gmail.com>
2024-08-25 00:43:11 +0200

    Added DeathListener

---
+++ src/main/java/cc/davyy/cduels/listeners/DeathListener.java
@@ -0,0 +1,26 @@
+package cc.davyy.cduels.listeners;
+
+import cc.davyy.cduels.managers.DuelManager;
+import com.google.inject.Inject;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+
+    private final DuelManager duelManager;
+
+    @Inject
+    public DeathListener(DuelManager duelManager) {
+        this.duelManager = duelManager;
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player winner = event.getPlayer();
+        Player loser = event.getEntity();
+        duelManager.endDuel(winner, loser);
+    }
+
+}
