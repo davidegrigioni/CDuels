740af848eeddf5fd7fbefb94225858a333f38cd5
davide <dav.grigioni@gmail.com>
2024-08-28 01:51:44 +0200

    Modified Messages Enum and added getStringMessage() Method for Strings

--- src/main/java/cc/davyy/cduels/utils/Messages.java
+++ src/main/java/cc/davyy/cduels/utils/Messages.java
@@ -1,5 +1,12 @@
 package cc.davyy.cduels.utils;
 
+import de.leonhard.storage.Yaml;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.jetbrains.annotations.NotNull;
+
+import static cc.davyy.cduels.utils.ConfigUtils.getConfig;
+
 public enum Messages {
 
     DUEL_STARTED("messages.duel-started"),
@@ -11,15 +18,27 @@ public enum Messages {
     DUEL_YOURSELF("messages.duel-yourself"),
     DUEL_INVITE_SEND("messages.duel-invite-send"),
     DUEL_INVITE_RECEIVED("messages.duel-invite-received"),
-    DUEL_ENDED("messages.duel-end"),
+    DUEL_WON("messages.duel-won"),
+    DUEL_NO_PENDING_REQUEST("messages.duel-no-pending"),
+    PLAYER_NO_LONGER_ONLINE("messages.player-no-longer-online"),
+    DUEL_CHALLENGE_ACCEPTED("messages.duel-challenge-accepted"),
+    DUEL_ACCEPTED("messages.duel-accepted"),
+    DUEL_LOST("messages.duel-lost"),
     DUEL_WORLD_CREATION_FAILED("messages.dw-creation-failed");
 
     private final String message;
+    private final static Yaml config = getConfig();
+    private static final MiniMessage MM = MiniMessage.miniMessage();
 
     Messages(String message) {
         this.message = message;
     }
 
-    public String getMessage() { return message; }
+    public @NotNull String getStringMessage() { return message; }
+
+    public @NotNull Component getMessage() {
+        return MM
+                .deserialize(config.get(message, message));
+    }
 
 }
